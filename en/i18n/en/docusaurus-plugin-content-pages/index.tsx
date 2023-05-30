b2874ed87958162fac6b57248efc252ef77e9b5d
李小雨 <xiaoyu@metalyoung.com>
2023-05-31 06:33:52 +0800

    New translations index.tsx (English)

--- en/i18n/en/docusaurus-plugin-content-pages/index.tsx
+++ en/i18n/en/docusaurus-plugin-content-pages/index.tsx
@@ -17,12 +17,19 @@ function HomepageHeader() {
       
         <h1 className="hero__title"><Translate>{siteConfig.title}</Translate></h1>
         
-        <p className="hero__subtitle"><Translate>你的智能待办事项管理助手</Translate></p>
+        <p className="hero__subtitle"><Translate>Your Smart To-do Manager Assistant</Translate></p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="https://forms.gle/VJtRDEhTVXzutRbu5">
-            加入等待名单 ⏱️
+            加入等待名单（google） ⏱️
+          </Link>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+          <Link
+            className="button button--secondary button--lg"
+            to="https://metaxiaoyu.feishu.cn/share/base/form/shrcnNoGjCBhPhRUNU2kJ2MkZne">
+            加入等待名单（飞书） ⏱️
           </Link>
         </div>
        
