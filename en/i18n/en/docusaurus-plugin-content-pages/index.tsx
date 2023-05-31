4bc9b8023341e5f78a796b02eab7d52d50399cf1
李小雨 <xiaoyu@metalyoung.com>
2023-05-31 12:04:03 +0800

    New Crowdin updates (#7)
    * Create source file 2019-05-28-first-blog-post.md
    
    * Create source file 2019-05-29-long-blog-post.md
    
    * Create source file 2021-08-01-mdx-blog-post.mdx
    
    * Update source file index.tsx
    
    * New translations index.tsx (English)
    
    * New translations index.module.css (English)
    
    * New translations index.tsx (English)
    
    * New translations intro.md (English)
    
    * Update source file intro.md
    
    * Update source file index.tsx
    
    * New translations index.tsx (English)
    
    * New translations markdown-page.md (English)
    
    * New translations intro.md (English)
    
    * Update source file authors.yml
    
    * New translations authors.yml (English)
    
    * New translations index.tsx (English)
    
    * Delete 2019-05-28-first-blog-post.md
    
    * Delete 2019-05-29-long-blog-post.md
    
    * Delete 2021-08-01-mdx-blog-post.mdx
    
    * New translations index.tsx (English)

---
+++ en/i18n/en/docusaurus-plugin-content-pages/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Translate from '@docusaurus/Translate';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+     
+      <div className="container">
+      
+        <h1 className="hero__title"><Translate>{siteConfig.title}</Translate></h1>
+        
+        <p className="hero__subtitle"><Translate>Your Smart To-do Manager Assistant</Translate></p>
+        <div className={styles.buttons}>
+      
+        <LinkJoinWaitlistComponent />;
+          
+        </div>
+       
+      </div>
+    
+    </header>
+  );
+}
+
+function LinkJoinWaitlistComponent() {
+  const { i18n } = useDocusaurusContext();
+  const { currentLocale } = i18n;
+
+  if (currentLocale.startsWith('zh')) {
+    return <Link
+    className="button button--secondary button--lg"
+    to="https://metaxiaoyu.feishu.cn/share/base/form/shrcnNoGjCBhPhRUNU2kJ2MkZne">
+    加入等待名单（飞书） ⏱️
+  </Link>;
+  } else {
+    return <Link
+    className="button button--secondary button--lg"
+    to="https://forms.gle/VJtRDEhTVXzutRbu5">
+    Join waitlist
+  </Link>;
+  }
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
