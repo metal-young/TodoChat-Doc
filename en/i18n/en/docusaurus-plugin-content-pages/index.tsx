c6399de80cac26b706c2705f29aea2411193efec
李小雨 <xiaoyu@metalyoung.com>
2023-05-31 14:45:18 +0800

    New translations index.tsx (English)

--- en/i18n/en/docusaurus-plugin-content-pages/index.tsx
+++ en/i18n/en/docusaurus-plugin-content-pages/index.tsx
@@ -56,28 +56,28 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: '专注于你的工作，提高你的效率',
+    title: 'Focus on your work, improve your efficiency',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
-      <>TodoChat结合了单核工作法和番茄工作法，帮助你更有效地管理你的时间和任务。你可以专注于一项任务，然后在完成任务后休息一会儿，这样可以提高你的工作效率，减少工作压力。
+      <>TodoChat combines monolithic and pomodoro methods to help you manage your time and tasks more effectively. You can focus on a task and rest for a short time after the task has been completed, thus improving your efficiency and reducing the pressure on your work.
       </>
     ),
   },
   {
-    title: '使用自然语言管理你的待办事项',
+    title: 'Manage your to-dos in Natural Language',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-TodoChat接入主流大语言模型，让你可以通过自然语言来创建、更新和管理你的待办事项。你只需要输入你的任务，TodoChat就会自动帮你安排和管理。
+TodoChat is connected to the mainstream language model so that you can create, update and manage your to-do using the natural language. You only need to enter your task and TodoChat will help you organize and manage it automatically.
       </>
     ),
   },
   {
-    title: '为你的生活和工作带来便利',
+    title: 'Make your life and work easier',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-无论你是在工作中，还是在生活中，TodoChat都可以帮助你管理你的任务。你可以随时随地查看你的待办事项，确保你不会错过任何重要的事情。
+Whether you work or live, TodoChat can help you manage your tasks. You can view your to-do anytime and anywhere, make sure you don't miss any important things.
       </>
     ),
   },
